5838b263ccd6026f4d360cddb7baaac822907e43
restrada94 <35005338+restrada94@users.noreply.github.com>
2018-04-25 23:40:03 -0500

    Add files via upload
    updates

--- javascript/googlemap.js
+++ javascript/googlemap.js
@@ -1,32 +1,19 @@
-var map;
-var marker;
-var current;
-
 var unitedStates=(lat: 37.0902, lng:-95.7128);
 var cholula={lat: 19.0599, lng: -98.2949};
 var tenochtitlan={lat: 19.2806, lng: -99.0588};
 
 var locations=["Cholula", "Tenochtitlan"];
 
-function myMap() {
-	current={lat: 41.8562277, lng: -87.6912785};
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom:9,
-	center: unitedStates});
-	marker = new google.maps.Marker({
-		position:unitedStates,
-		animation:google.maps.Animation.BOUNCE,
-	map: map });
+function mapFunction() {
+	var current={lat: 41.8562277, lng: -87.6912785};
+	var map = new google.maps.Map(document.getElementById('map'), {center: unitedStates, zoom:9});
+	var marker = new google.maps.Marker({map: map, position:unitedStates, title: 'USA'});
 }
 
-function travel(location, my_locations) {
+function locationFunction(location, my_locations) {
 	map.panTo(location);
-	map.setZoom(15);
-	marker = new google.maps.Marker({
-		position: location,
-			animation:google.maps.Animation.BOUNCE,
-		map: map
-	});
+	map.setZoom(14);
+	marker = new google.maps.Marker({map: map, position: location, title: location});
 	if(my_locations==locations[0]){
 		document.getElementById("description").innerHTML="Cholula"
 	}
